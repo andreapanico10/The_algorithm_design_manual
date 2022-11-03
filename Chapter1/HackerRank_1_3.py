5841e3bc6bfd09e98e56b78cf1a939fb062d2bf5
andreapanico10 <andrea.panico4@studenti.unisalento.it>
2022-11-03 19:44:41 +0100

    Create HackerRank_1_3.py
    COME BACK AFTER DYNAMIC PROGRAMMING

---
+++ Chapter1/HackerRank_1_3.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+#COME BACK WITH Dynamic programming
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hackerlandRadioTransmitters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY x
+#  2. INTEGER k
+#
+
+def hackerlandRadioTransmitters(x, k):
+    x.sort()
+    n = max(x);
+    bitmap = [0]*n
+    for house in x:
+        bitmap[house-1] = 1
+    print(bitmap)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    x = list(map(int, input().rstrip().split()))
+
+    result = hackerlandRadioTransmitters(x, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
